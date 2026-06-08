61cab0cbbe8803b307604c464195fa6d110ff110
programmersajeeb <sajeeb.programmer@gmail.com>
2026-06-09 00:50:25 +0600

    add Suspense

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -1,6 +1,12 @@
+import { Suspense } from "react";
+
 import { HeaderClient } from "./header/header-client";
 import { headerData, headerSearchConfig } from "./header/header-data";
 
 export function SiteHeader() {
-  return <HeaderClient data={headerData} searchConfig={headerSearchConfig} />;
+  return (
+    <Suspense fallback={null}>
+      <HeaderClient data={headerData} searchConfig={headerSearchConfig} />
+    </Suspense>
+  );
 }
